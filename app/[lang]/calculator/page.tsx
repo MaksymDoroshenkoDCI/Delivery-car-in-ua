7b1a5256a5affc0b5185c4b7239bd44e62997e5c
Maksym Doroshenko <maksym.doroshenko@dci-student.org>
2025-09-22 17:42:10 +0200

    Додав сторінку калькулятор

--- app/[lang]/calculator/page.tsx
+++ app/[lang]/calculator/page.tsx
@@ -1,8 +1,16 @@
 import { getDictionary } from "../dictionaries";
 import Calculator from "./Calculator";
-
-export default function Page({ params }: { params: { lang: string } }) {
-  const dict = getDictionary(params.lang as "ua" | "ru" | "en");
-
-  return <Calculator dict={dict} />;
+import React from "react";
+import Layout from "@/components/layout";
+interface PageProps {
+  params: { lang: "ua" | "ru" | "en" };
 }
+
+export default async function Page({ params }: PageProps) {
+  const dict = await getDictionary(params.lang);
+  return (
+    <Layout lang={params.lang} dict={dict}>
+      <Calculator dict={dict} />
+    </Layout>
+  );
+} 
